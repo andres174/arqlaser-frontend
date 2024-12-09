24ca061d47dec5ba8e8659b96ba534be3468db36
gabriel12qk <jogar737@gmail.com>
2024-12-09 09:30:49 -0500

    instalacion de prime ng y configuracion de Sakai

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
+import { HomeComponent } from './core/admin/home/home.component';
+import { HomeUsuarioComponent } from './core/user/home-usuario/home-usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin',
+        component: AppLayoutComponent,
+        children:[
+            { path:'', component:HomeComponent }
+        ],
+        
+    },
+    {
+        path:'',
+        component: HomeUsuarioComponent 
+    }
+];
---
+++ src/app/core/user/home-usuario/home-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-usuario.component.html',
+  styleUrl: './home-usuario.component.css'
+})
+export class HomeUsuarioComponent {
+
+}
