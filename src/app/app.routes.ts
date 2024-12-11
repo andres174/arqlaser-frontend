a68cb5ecc2561d3d14bdba3f8c5e1595e19195db
gabriel12qk <jogar737@gmail.com>
2024-12-11 10:47:52 -0500

    preliminar de registro y login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { AppLayoutComponent } from './layout/app.layout.component';
 import { HomeComponent } from './core/admin/home/home.component';
 import { HomeUsuarioComponent } from './core/user/home-usuario/home-usuario.component';
 import { AppLayoutUserComponent } from './userLayout/app-layout-user/app-layout-user.component';
+import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
 
 export const routes: Routes = [
     {
@@ -20,5 +22,14 @@ export const routes: Routes = [
         children:[
             {path:'', component:HomeUsuarioComponent}
         ] 
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'registro',
+        component:RegisterComponent
     }
+
 ];
