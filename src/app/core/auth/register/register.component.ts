e6736565ba70f479d447da2d3e057a76a80ded20
gabriel12qk <jogar737@gmail.com>
2024-12-18 10:59:07 -0500

    mensajes de error en registro

--- src/app/core/auth/register/register.component.ts
+++ src/app/core/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2'
@@ -13,28 +13,42 @@ import Swal from 'sweetalert2'
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  registerForm= signal<FormGroup>(
-      new FormGroup({
-        nombre: new FormControl('',[Validators.required,]),
-        apellido: new FormControl('',[Validators.required]),
-        email: new FormControl('',[Validators.required, Validators.email]),
-        password: new FormControl('',[Validators.minLength(8), Validators.required]),
-      })
-    )
-
+  registerForm!: FormGroup
+  statusForm: Boolean = true
+  showPassword: boolean = false;
    constructor(
       private authService:AuthService,
       private router:Router,
-    ){}
+      private formBuilder: FormBuilder
+    ){
+      this.buildForm()
+    }
+     
+    
   
     ngOnInit(){
-  
+      this.statusForm=true
+    }
+
+
+
+    buildForm(){
+      this.registerForm = this.formBuilder.group({
+        nombre:['',[Validators.required,]],
+        apellido:['',[Validators.required,]],
+        email:['',[Validators.required,Validators.email]],
+        password:['',[Validators.required,Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,20}$/)]],
+      })
+    }
+
+    changeVisibility() {
+      this.showPassword = !this.showPassword; 
     }
 
     register(){
-      if (this.registerForm().valid) {
+      if (this.registerForm.valid) {
         console.log('entro')
-            this.authService.Register(this.registerForm().value).subscribe({
+            this.authService.Register(this.registerForm.value).subscribe({
               next:(res) =>{
                 console.log(res)
                 this.authService.guardarToken(res)
@@ -49,12 +63,16 @@ export class RegisterComponent {
               },
               error: (err)=>{
                 console.log(err);
-                
               }
             })
           }
+          else{
+            this.statusForm=false
+          }
          
         }
-  
+ get getControls() {
+          return this.registerForm.controls
+  }
 
 }
